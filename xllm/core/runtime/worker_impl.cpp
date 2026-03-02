2ed20b90db22687742b1041200ede8068881787f
shenxiaolong <1193789086@qq.com>
2026-03-02 10:12:22 +0800

    feat: support qwen3-next inference on npu.
    feat: adjust cache allocation based on attention settings(support ssm_cache).
    
    feat: update torch_npu_ops commit.
    
    bugfix:layer CMake fix.
    
    bugfix:add model arguments for enhanced configuration.
    
    bugfix: QKV linear load fix.
    
    bugfix: fallback pg_comm.
    
    bugfix: set q_seq_len in prefill.
    
    bugfix: handle optional finished tensor in moe gating and FusedMoE implementations.
    
    bugfix: ensure activation output is correctly assigned in FusedMoE forward pass.

--- xllm/core/runtime/worker_impl.cpp
+++ xllm/core/runtime/worker_impl.cpp
@@ -134,7 +134,13 @@ bool WorkerImpl::allocate_kv_cache(
     const std::vector<std::vector<int64_t>>& kv_cache_shape) {
   CHECK(model_ != nullptr) << "Model is not initialized.";
   CHECK(kv_caches_.empty()) << "KV caches are already initialized.";
-  const bool enable_linear_attention = context_.get_model_args().full_attention_interval() > 1;
+  const bool enable_linear_attention =
+      context_.get_model_args().full_attention_interval() > 1;
+  const bool enable_lighting_indexer =
+      context_.get_model_args().index_n_heads() > 0;
+  CHECK(!(enable_linear_attention && enable_lighting_indexer))
+      << "KVCache does not support linear attention and lighting indexer "
+      << "simultaneously.";
 
   // Check if KV cache quantization is enabled
   // "auto" (default): cache dtype aligns with model dtype (no quantization)
@@ -159,8 +165,6 @@ bool WorkerImpl::allocate_kv_cache(
 
   // create a KVCache for each layer
   const int64_t num_layers = get_num_layers();
-  const bool enable_lighting_indexer =
-      context_.get_model_args().index_n_heads() > 0;
   kv_caches_.reserve(num_layers);
 
   // Determine cache dtype based on quantization setting
@@ -234,14 +238,19 @@ bool WorkerImpl::allocate_kv_cache(
           torch::empty(kv_cache_shape[2], torch::dtype(dtype_).device(device_));
     }
 #endif
-    if (enable_kv_cache_quant) {
+
+    if (enable_linear_attention) {
+      kv_caches_.emplace_back(key_cache, value_cache, conv_cache, ssm_cache);
+    } else if (enable_lighting_indexer) {
+      kv_caches_.emplace_back(key_cache, value_cache, index_cache);
+    } else if (enable_kv_cache_quant) {
       kv_caches_.emplace_back(key_cache,
                               value_cache,
                               index_cache,
                               key_cache_scale,
                               value_cache_scale);
     } else {
-      kv_caches_.emplace_back(key_cache, value_cache, index_cache, conv_cache, ssm_cache);
+      kv_caches_.emplace_back(key_cache, value_cache);
     }
   }
 
